7e499b1d6c65a5f8f27a367ecedc41b1de8fa950
Alex Provan <s4251450@student.rmit.edu.au>
2026-02-25 18:37:18 +1100

    Completed Week 3 completed code

---
+++ Week3/Colours.py
@@ -0,0 +1,9 @@
+class Colours:
+    @staticmethod
+    def main():
+        print("\033[31m This is some red text\033[0m")
+        print("\033[34m This is some blue text\033[0m")
+        print("\033[41m This is red highlighted text\033[0m")
+        print("\033[44m This is blue highlighted text\033[0m")
+        print("Hello World")
+Colours.main()
